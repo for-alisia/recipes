c67a9aeb7f52045bc73c8f57cac7e74ff326a0f6
for-alisia <for-alisia@yandex.ru>
2020-06-27 15:48:10 +0500

    Created base layout for the components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'An awesome burger',
+      "The tastest burger you've ever eat",
+      'https://image.freepik.com/free-photo/delicious-beef-burgers-wooden-board_23-2148290634.jpg'
+    ),
+    new Recipe(
+      'An awesome pizza',
+      "The tastest pizza you've ever eat",
+      'https://image.freepik.com/free-photo/pizza-pizza-filled-with-tomatoes-salami-olives_140725-1200.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
