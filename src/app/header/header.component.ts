f42469aa51692b1ff577d704b2fbd4456871ef21
for-alisia <for-alisia@yandex.ru>
2020-06-27 14:40:16 +0500

    Initial commit with basic structure of a project

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.comonent.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  constructor() {}
+}
